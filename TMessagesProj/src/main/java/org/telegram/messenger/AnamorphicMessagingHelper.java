e7ad1a5d495450ade585255e93fd874016fa3ea0
TheDavidNN <davn@itu.dk>
2026-04-05 21:12:14 +0200

    Create anamorphic helper class
    (cherry picked from commit 6447fb6352728931ea9ba54fd7cea1b1a0c97890)

---
+++ TMessagesProj/src/main/java/org/telegram/messenger/AnamorphicMessagingHelper.java
@@ -0,0 +1,115 @@
+package org.telegram.messenger;
+
+import com.google.android.exoplayer2.util.Log;
+
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+
+public class AnamorphicMessagingHelper {
+    // Class private variables
+    private static final String SECRET_KEY
+            = "my_super_secret_key_ho_ho_ho";
+
+    private static final String SALT = "ssshhhhhhhhhhh!!!!";
+
+    // This method use to encrypt to string
+    public static byte[] encrypt(String strToEncrypt)
+    {
+        try {
+
+            // Create default byte array
+            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0 };
+            IvParameterSpec ivspec
+                    = new IvParameterSpec(iv);
+
+            // Create SecretKeyFactory object
+            SecretKeyFactory factory
+                    = SecretKeyFactory.getInstance(
+                    "PBKDF2WithHmacSHA256");
+
+            // Create KeySpec object and assign with
+            // constructor
+            KeySpec spec = new PBEKeySpec(
+                    SECRET_KEY.toCharArray(), SALT.getBytes(),
+                    65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(
+                    tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance(
+                    "AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey,
+                    ivspec);
+
+            // Return encrypted string
+            byte[] b = strToEncrypt.getBytes(StandardCharsets.UTF_8);
+
+            return cipher.doFinal(b);
+        }
+        catch (Exception e) {
+            System.out.println("Error while encrypting: "
+                    + e.toString());
+        }
+        return null;
+    }
+
+    // This method use to decrypt to string
+    public static String decrypt(byte[] strToDecrypt)
+    {
+        try {
+
+            // Default byte array
+            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0, 0, 0 };
+
+            // Create IvParameterSpec object and assign with
+            // constructor
+            IvParameterSpec ivspec
+                    = new IvParameterSpec(iv);
+
+            // Create SecretKeyFactory Object
+            SecretKeyFactory factory
+                    = SecretKeyFactory.getInstance(
+                    "PBKDF2WithHmacSHA256");
+
+            // Create KeySpec object and assign with
+            // constructor
+            KeySpec spec = new PBEKeySpec(
+                    SECRET_KEY.toCharArray(), SALT.getBytes(),
+                    65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(
+                    tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance(
+                    "AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey,
+                    ivspec);
+            // Return decrypted string
+            return new String(cipher.doFinal(strToDecrypt));
+        }
+        catch (Exception e) {
+            System.out.println("Error while decrypting: "
+                    + e.toString());
+        }
+        return null;
+    }
+}
